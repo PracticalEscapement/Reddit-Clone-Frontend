2748e4d5c9ab0f5982ec483550b85a31dc340334
PracticalEscapement <themoradis1@gmail.com>
2025-05-06 18:55:51 -0400

    Add User profile card

---
+++ src/lib/fakeUser.js
@@ -0,0 +1,17 @@
+// src/lib/fakeUser.js (optional mock)
+export const getUserProfile = async () => {
+    return [
+      {
+        id: 1,
+        username: 'oscar2 dejesus2',
+        email: 'oscar2@gmail.com',
+        comments: [],
+        posts: [],
+        communities: [],
+        notifications: [],
+        sent_messages: [],
+        recieved_messages: [],
+      }
+    ];
+  };
+  
