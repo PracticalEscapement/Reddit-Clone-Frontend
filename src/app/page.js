9d083e095b7c44c069e1b505ac37d237004d0ddf
colaola20 <soryo@farmingdale.edu>
2025-05-12 13:13:25 -0400

    fixes to fetching community feature

--- src/app/page.js
+++ src/app/page.js
@@ -97,7 +97,7 @@ export default function Home() {
           key={community.name}
           title={community.name}
           body={community.description}
-          imageUrl={community.imageUrl || null}
+          imageUrl={community.image_url || null}
         />
       ))}
 
