7e876ad2bc0a5cecfd49a29d7eee81c2ab6443be
colaola20 <soryo@farmingdale.edu>
2025-05-12 14:31:25 -0400

    updates to PostCard visuality

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -5,12 +5,12 @@ export default function PostCard({ title, body, imageUrl, members }) {
     <div className="post-card">
       <h2 className="post-title">{title}</h2>
       <p className="post-body">{body}</p>
-      <p className="post-members">{members} members</p> {/* Add members info */}
       {imageUrl && (
         <div className="post-image-wrapper">
           <img src={imageUrl} alt="Post" className="post-image" />
         </div>
       )}
+      <p className="post-members">{members} members</p> {/* Add members info */}
     </div>
   );
 }
